f811a954bd358136bf0c727191486cafd87ea316
Ian McPhail <ian@clerk.dev>
2022-06-29 10:45:15 -0400

    fix: Update request example to return userId

--- pages/api/getAuthenticatedUserId.js
+++ pages/api/getAuthenticatedUserId.js
@@ -1,11 +1,11 @@
 import { withAuth } from "@clerk/nextjs/api";
 
 export default withAuth((req, res) => {
-  const { sessionId } = req.auth;
+  const { sessionId, userId } = req.auth;
 
   if (!sessionId) {
     return res.status(401).json({ id: null });
   }
 
-  return res.status(200).json({ id: sessionId });
+  return res.status(200).json({ id: userId });
 });
--- pages/index.js
+++ pages/index.js
@@ -73,13 +73,13 @@ const apiSample = `
 import { withAuth } from "@clerk/nextjs/api";
 
 export default withAuth((req, res) => {
-  const { sessionId } = req.auth;
+  const { sessionId, userId } = req.auth;
 
   if (!sessionId) {
     return res.status(401).json({ id: null });
   }
 
-  return res.status(200).json({ id: sessionId });
+  return res.status(200).json({ id: userId });
 });
 `.trim();
 
