525695abb8a1a42bf572148d4df94658a553fc59
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-07-26 12:50:48 +0300

    Replace _middleware with root middleware

--- middleware.js
+++ middleware.js
@@ -1,16 +1,14 @@
 import { withEdgeMiddlewareAuth } from "@clerk/nextjs/edge-middleware";
 import { NextResponse } from "next/server";
 
-export default withEdgeMiddlewareAuth((request) => {
+export const middleware = withEdgeMiddlewareAuth((request) => {
   const { sessionId, userId } = request.auth;
 
-  if (!sessionId) {
+  if (request.nextUrl.pathname.startsWith("/protected-pages") && !sessionId) {
     const destination = request.nextUrl.href;
     const url = request.nextUrl.clone();
     url.pathname = "/sign-in";
     url.searchParams.set("redirect_url", destination);
     return NextResponse.redirect(url);
   }
-
-  return NextResponse.next();
 });
