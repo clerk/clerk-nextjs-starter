09b02dcb0512c5d67a262c1d94c05338feb98fdd
Dev Agrawal <dev@clerk.dev>
2023-05-11 10:57:45 -0400

    new middleware

--- middleware.js
+++ middleware.js
@@ -1,30 +1,7 @@
-import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-// Set the paths that don't require the user to be signed in
-const publicPaths = ['/', '/sign-in*', '/sign-up*']
+export default authMiddleware();
 
-const isPublic = (path) => {
-  return publicPaths.find(x =>
-    path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
-  )
-}
-
-export default withClerkMiddleware((request) => {
-  if (isPublic(request.nextUrl.pathname)) {
-    return NextResponse.next()
-  }
-  // if the user is not signed in redirect them to the sign in page.
-  const { userId } = getAuth(request)
-
-  if (!userId) {
-    // redirect the users to /pages/sign-in/[[...index]].ts
-    
-    const signInUrl = new URL('/sign-in', request.url)
-    signInUrl.searchParams.set('redirect_url', request.url)
-    return NextResponse.redirect(signInUrl)
-  }
-  return NextResponse.next()
-})
-
-export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico|.*\.svg).*)'};
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+};
