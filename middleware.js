cd1da0dcc4a85bd5a60927c848da07e6ca8c1960
Dev Agrawal <dev@clerk.dev>
2023-05-11 12:11:03 -0400

    configured the auth middleware with public route

--- middleware.js
+++ middleware.js
@@ -1,7 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  // Set the paths that don't require the user to be signed in
+  // Sign in and sign up pages are already made public by Clerk
+  publicRoutes: ["/"],
+});
 
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
-};
+export const config = { matcher: "/((?!_next/image|_next/static|favicon.ico|.*.svg).*)" };
